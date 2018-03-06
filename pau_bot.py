7ebeff1d11231dcaf3c07f7654c32d4bc9e4b489
FofiHdeza <36669687+FofiHdeza@users.noreply.github.com>
2018-03-06 10:14:45 -0400

    Update pau_bot.py

--- pau_bot.py
+++ pau_bot.py
@@ -1,1 +1,10 @@
-# code here
+import datetime
+
+def login(user):
+    if user.isdigit():
+        if int(l[0:4]) >= 1962 and len(l)==8 and datetime.datetime.now().year >= int(l[0:4]):
+            return "aquivaelcodigosiguiente"
+        else:
+            return ("Matrícula invalida")
+    else:
+        return ("Ingrese una matricula valida")
