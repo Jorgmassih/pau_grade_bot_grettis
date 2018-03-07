2055749bfbf6a4105ac0d09f46e4d50ddd7b20a5
jorgmassih <jorgmassih@gmail.com>
2018-03-07 01:05:33 -0400

    Last commit (i hope it)

--- pau_bot.py
+++ pau_bot.py
@@ -1,46 +1,39 @@
 import datetime
+import os.path
 
-def login(user):
-    if user.isdigit():
-        if int(l[0:4]) >= 1962 and len(l)==8 and datetime.datetime.now().year >= int(l[0:4]):
-            return "aquivaelcodigosiguiente"
-        else:
-            return ("Matrícula invalida")
-    else:
-        return ("Ingrese una matricula valida")
+def login(id):
+    if id.isdigit() and int(id[0:4]) >= 1962 and len(id)==8 and datetime.datetime.now().year >= int(id[0:4]):
+        return True
+    else: return False
 
-def print_in_csv(id, text):
-    with open(id + "_summary" + ".csv", "a+") as generatedcsv:
-        return generatedcsv.write(text + "\n")
-
-
-def files_manager(exercises='result', u_exercise='fofi'):
-    with open(exercises + '.csv') as csvfile:
+def files_manager(id):
+    with open('codewars.csv') as csvfile:
         c_exercise = []
         channel = csvfile.readlines()
         for line in channel:
             if line[0].isnumeric(): c_exercise.append(line.split(","))
-        print(c_exercise)
+        # print(c_exercise)
 
-    with open(u_exercise + '.csv') as csvfile:
+    with open('exercises.csv') as csvfile:
         user_exercise = []
         channel = csvfile.readlines()
         for line in channel:
             if line[0].isnumeric(): user_exercise.append(line.split(","))
-        print(user_exercise)
+        # print(user_exercise)
 
     test = False
-    id = "2016-5459"
+
+
     for element in c_exercise:
         for element_2 in user_exercise:
             if element[3] == 'https://www.codewars.com/kata/' + element_2[2]:
                 test = True
                 if datetime.datetime.strptime(element_2[4][0:8], '%m/%d/%y') <= datetime.datetime.strptime(element[1], '%m/%d/%y'):
-                    status = "{},{},{},{},{}".format(element[0], element_2[2], True, element_2[4][:8], True)
+                    status = "{},{},{},{},{}".format(element[0], element_2[2], True, element_2[4][:8], False)
                     print(status)
                     print_in_csv(id, status)
                 else:
-                    status = "{},{},{},{},{}".format(element[0], element_2[2], True, element_2[4][:8], False)
+                    status = "{},{},{},{},{}".format(element[0], element_2[2], True, element_2[4][:8], True)
                     print(status)
                     print_in_csv(id, status)
 
@@ -50,5 +43,56 @@ def files_manager(exercises='result', u_exercise='fofi'):
             print_in_csv(id, status)
     return ""
 
+def print_in_csv(id, text):
+    with open('processed.csv', "a+") as generatedcsv:
+        return generatedcsv.write(text + "\n")
 
-print(files_manager())
+def found_file_test(id_test):
+  file = id_test + '.csv'
+  if os.path.exists(file): return True
+  else: return False
+
+def summary(id):
+    with open('processed.csv') as csvfile:
+        list = []
+        channel = csvfile.readlines()
+        for line in channel:
+            list.append(line.split(","))
+        print(list)
+
+    count = 0
+    elements = 0
+    for element in list:
+        elements+=1
+        if element[4][:4] == "True": count+=1
+
+    print("""
+StudentId: {}
+TotalExcercises: {}        
+TotalCompleted: {}
+TotalLate: {}
+TotalMissing: {}
+    """.format(id, 37, len(list), count, 37-len(list)))
+
+
+option = input("""Seleccione una opcion:
+1. Login
+2. Exit
+""")
+
+if option == "1":
+    while True:
+        input_ = input("Ingrese su matricula: ")
+        if login(input_)==True: break
+        else: print("Matricula incorrecta, inserte un ID válido.\n")
+
+    while True:
+        option_2 = input("""
+Seleccione una funcion
+1. Codewars
+2. Summary
+        """)
+        if option_2 == "1":
+            print(files_manager(input_))
+
+        if option_2 == "2": summary(input_)
